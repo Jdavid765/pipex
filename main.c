c329888a945d2c331bbb7274fd839a5dbc0432b1
Jdavid765 <david.canoduran@hotmail.com>
2025-12-03 17:33:29 +0100

    continue

--- main.c
+++ main.c
@@ -6,14 +6,30 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 18:32:55 by david             #+#    #+#             */
-/*   Updated: 2025/11/26 20:44:18 by david            ###   ########.fr       */
+/*   Updated: 2025/12/03 16:39:17 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	int pipefd[0];
-	int pipefd[1]
+	char *cpy = av[0];
+	printf("%s\t", cpy);
+	if (ac == 1)
+	{
+		int	id = fork();
+
+		if (id == 0)
+		{
+			printf("The process child\n");
+		}
+		else
+		{
+			printf("The process main\n");
+		}
+	}
+	else
+		printf("\n");
+	return (0);
 }
