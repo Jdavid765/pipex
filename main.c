5d118e92d268716c709f122565ef359312f6a8c5
Jdavid765 <david.canoduran@hotmail.com>
2025-12-09 00:53:46 +0100

    continue

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 18:32:55 by david             #+#    #+#             */
-/*   Updated: 2025/12/09 00:23:42 by david            ###   ########.fr       */
+/*   Updated: 2025/12/09 00:52:37 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ int main(void)
 {
     int fd;
 	int fd_cpy;
-	char buffer[60];
+	char buffer[40];
 
     fd = open("infile", O_RDONLY);
     if (fd == -1)
 		return (-1);
 	int nb_bytes = read(fd, buffer, 30);
 	buffer[nb_bytes] = '\0';
-	ft_printf("%s", buffer);
+	ft_printf("fd = %d, %s", fd, buffer);
 	fd_cpy = dup(fd);
 	nb_bytes = read(fd_cpy, buffer, 30);
 	buffer[nb_bytes] = '\0';
-	ft_printf("%s", buffer);
+	ft_printf("fd = %d, %s", fd_cpy ,buffer);
 	close (fd);
 	close (fd_cpy);
     return (0);
