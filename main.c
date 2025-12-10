947a91fe9052960700d4e3ca10534f2e98793828
Jdavid765 <david.canoduran@hotmail.com>
2025-12-10 21:18:56 +0100

    i go ez this

--- main.c
+++ main.c
@@ -6,29 +6,28 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 18:32:55 by david             #+#    #+#             */
-/*   Updated: 2025/12/09 00:52:37 by david            ###   ########.fr       */
+/*   Updated: 2025/12/10 21:17:22 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main(void)
+int main(int ac, char *av[], char *env[])
 {
-    int fd;
-	int fd_cpy;
-	char buffer[40];
+	pid_t	pid;
 
-    fd = open("infile", O_RDONLY);
-    if (fd == -1)
-		return (-1);
-	int nb_bytes = read(fd, buffer, 30);
-	buffer[nb_bytes] = '\0';
-	ft_printf("fd = %d, %s", fd, buffer);
-	fd_cpy = dup(fd);
-	nb_bytes = read(fd_cpy, buffer, 30);
-	buffer[nb_bytes] = '\0';
-	ft_printf("fd = %d, %s", fd_cpy ,buffer);
-	close (fd);
-	close (fd_cpy);
+	if (ac > 1)
+	{
+		pid = fork();
+		if (pid == -1)
+			perror("fork :");
+		if (pid == 0)
+		{
+			if (execve(av[1], av + 1, env) == -1)
+				perror("fork : ");
+		}
+		else
+			ft_printf("Je suis ton pere\n");
+	}
     return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 18:33:04 by david             #+#    #+#             */
-/*   Updated: 2025/12/08 23:11:17 by david            ###   ########.fr       */
+/*   Updated: 2025/12/10 18:10:45 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# include <stdio.h>
 # include "printf/ft_printf.h"
 
 #endif
