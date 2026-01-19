32a7dec157f0605889debfd9e1d110ff9915e217
Jdavid765 <david.canoduran@hotmail.com>
2026-01-19 16:30:30 +0100

    add permisioon

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 18:32:55 by david             #+#    #+#             */
-/*   Updated: 2026/01/19 16:26:09 by david            ###   ########.fr       */
+/*   Updated: 2026/01/19 16:30:04 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	child_one(char **av, char **env, int *pipefd)
 
 	path = NULL;
 	close(pipefd[0]);
-	fd = open(av[1], O_RDONLY | O_CREAT);
+	fd = open(av[1], O_RDONLY | O_CREAT, 0644);
 	if (fd == -1)
 	{
 		perror("Fd :");
@@ -45,7 +45,7 @@ void	child_two(char **av, char **env, int *pipefd)
 
 	path = NULL;
 	close(pipefd[1]);
-	fd = open(av[4], O_WRONLY | O_CREAT);
+	fd = open(av[4], O_WRONLY | O_CREAT, 0644);
 	if (fd == -1)
 	{
 		perror("Fd :");
